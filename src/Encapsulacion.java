fc8390796f20928701e28be18ab170952c61cee4
Jose <calamarte@gmail.com>
2019-11-21 21:53:11 +0100

    Ejemplo encapsulacion de codigo

---
+++ src/Encapsulacion.java
@@ -0,0 +1,28 @@
+public class Encapsulacion {
+    public static void main(String[] args) {
+
+        say("hola", 1);
+
+    }
+
+
+    public static void say(String msg, int repeat){
+        for (int i = 0; i < repeat; i++) {
+            say(msg);
+        }
+    }
+
+    public static void say(String msg){
+        System.out.println(getOSName() + " say: " + msg);
+    }
+
+    public static String getOSName(){
+
+        String name = System.getProperty("os.name");
+
+        return name;
+    }
+
+
+
+}
